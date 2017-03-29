6ab271883934fcd59e78145a4b86b10f35cc7588
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-29 18:41:42 -0400

    add hash for each cat

--- cat.rb
+++ cat.rb
@@ -1,14 +1,29 @@
 class Cat
+  #feline
+cat1 = [:name => "Sylvester", :preferred_food => "Tuna", :meal_time => 11]
+cat2 = [:name => "Tom", :preferred_food => "Chicken", :meal_time => 16]
+cat3 = [:name => "Billy", :preferred_food => "Beef", :meal_time => 22]
 
-attr_accessor :time
 
-
-  def meal_time
-    if
-      @time.to_i > 12
-      print "AM"
-    else
-      print "PM"
+  def intialize
+    @name
+    @preferred_food
+    @meal_time
   end
-
 end
+
+
+
+#
+# attr_accessor :time
+#
+#
+#   def meal_time
+#     if
+#       @time.to_i > 12
+#       print "AM"
+#     else
+#       print "PM"
+#   end
+#
+# end
