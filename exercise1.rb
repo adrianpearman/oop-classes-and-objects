481c4bf6f7b510f0577327b9fae351e5a0a21d7d
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-30 16:34:14 -0400

    finish assignment 1

--- exercise1.rb
+++ exercise1.rb
@@ -12,6 +12,8 @@ class BankAccount
     end
 
     def withdrawl(amount)
+      if @balance - amount < 0
+        puts "Insufficient funds for this amount"
       @balance = @balance - amount
     end
 
@@ -26,6 +28,7 @@ cheq_acct.deposit(900)
 cheq_acct.withdrawl(400)
 cheq_acct.interest_rate(3)
 puts "your balance is $#{ cheq_acct.balance }"
+
 
 # balance = BankAccount.new
 # interest_rate = BankAccount.new
