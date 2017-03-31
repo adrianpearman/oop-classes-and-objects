fdb1f8f8bc4a8fa72597cefe1c2d93c1111cccc4
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-31 16:36:19 -0400

    complete cat assignment

--- cat.rb
+++ cat.rb
@@ -1,29 +1,28 @@
-class Cat
-  #feline
-cat1 = [:name => "Sylvester", :preferred_food => "Tuna", :meal_time => 11]
-cat2 = [:name => "Tom", :preferred_food => "Chicken", :meal_time => 16]
-cat3 = [:name => "Billy", :preferred_food => "Beef", :meal_time => 22]
+class Cats
 
+  attr_accessor :name
+  attr_accessor :preferred_food
+  attr_accessor :meal_time
+  cat1 = Cats.new("Gary", "Tuna", 16)
+  cat2 = Cats.new("Bruce", "Chicken", 12)
+  cat3 = Cats.new("Reese", "Beef", 6)
 
-  def intialize
-    @name
-    @preferred_food
-    @meal_time
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
   end
+
+  def eats_at
+    if @meal_time.to_i < 12
+      return "#{meal_time} AM"
+    else
+      return "#{@meal_time.to_i - 12} PM"
+    end
+  end
+
+  def meow
+    print "My name is #{name} and I eat #{preferred_food} at #{ eats_at }"
+  end
+
 end
-
-
-
-#
-# attr_accessor :time
-#
-#
-#   def meal_time
-#     if
-#       @time.to_i > 12
-#       print "AM"
-#     else
-#       print "PM"
-#   end
-#
-# end
